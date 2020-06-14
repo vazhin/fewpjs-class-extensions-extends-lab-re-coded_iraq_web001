5cb0421e2e628f994bb318d79259c012fe5b32a2
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-14 21:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 class Polygon {
   constructor(array){
-    this.side = array;
+    this.sides = array;
+  }
+  get countSides() {
+    return this.sides.length
+  }
+  get perimeter(){
+    return this.sides.reduce((sum, side) => sum += side, 0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    return 
   }
 }
